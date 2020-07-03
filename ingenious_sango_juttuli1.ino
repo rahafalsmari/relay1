6c9c5f42092291789d30b93340aef58e852a20c5
rahafalsmari <67207495+rahafalsmari@users.noreply.github.com>
2020-07-03 16:38:14 +0300

    Add files via upload

---
+++ ingenious_sango_juttuli1.ino
@@ -0,0 +1,23 @@
+
+
+  
+
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+}
